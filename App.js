cbac6b3670eb941b21c15a2737298f70a1d95754
leygallego <leyder.gallego@gmail.com>
2022-01-26 17:20:02 -0500

    Firebase

--- App.js
+++ App.js
@@ -13,17 +13,7 @@ import Login from './components/Login';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA-4mFM_oNT-xk-1G1fCHVlLcCfBtpedww",
-//   authDomain: "virtualcc-b41d9.firebaseapp.com",
-//   projectId: "virtualcc-b41d9",
-//   storageBucket: "virtualcc-b41d9.appspot.com",
-//   messagingSenderId: "402994710782",
-//   appId: "1:402994710782:web:c838f2b58a1fcd2329bdaf",
-//   measurementId: "G-G3Y6W3EG3L"
-// };
+
 
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
